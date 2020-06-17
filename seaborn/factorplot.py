1eaeeeafde286178e51bd58811736e08aa6530dd
unknown <damodar2dikonda@gmail.com>
2020-06-17 16:43:58 +0530

    python program

---
+++ seaborn/factorplot.py
@@ -0,0 +1,15 @@
+#Factorplot draws a categorical plot on a FacetGrid. Using ‘kind’ parameter we
+#can choose the plot like boxplot, violinplot, barplot and stripplot. FacetGrid uses pointplot by default.
+
+import pandas as pd
+import seaborn as sb
+from matplotlib import pyplot as plt
+df = sb.load_dataset('exercise')
+sb.factorplot(x = "time", y = pulse", hue = "kind",data = df);
+plt.show()
+
+
+print('if you want to print as violine')
+df1 = sb.load_dataset('exercise')
+sb.factorplot(x = "time", y = pulse", hue = "kind",kind="violine",data = df1);
+plt.show()
